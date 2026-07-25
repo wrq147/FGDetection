120937be2d78f4dff1001b7082c66ba79858625e
wrq147 <53035636+wrq147@users.noreply.github.com>
2026-07-25 16:24:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -75,18 +75,24 @@ class AdaptedDetectHead(nn.Module):
             (sim_max_max - sim_max_min + 1e-8)
         real_mm = confidence * sim_mean
         mask = real_mm.unsqueeze(-1)          # [B,784,1]
-        cls_feat = fc_img_feat * mask  # [B, 784, 768]
-        cls_feat = cls_feat.permute(
+
+        mask_feat = fc_img_feat * mask  # [B, 784, 768]
+        cls_feat = mask_feat.permute(
             0, 2, 1).reshape(B, -1, featsize, featsize)
 
+        box_feat = mask_feat+fc_img_feat
+        box_feat = box_feat.permute(
+            0, 2, 1).reshape(B, -1, featsize, featsize)
 
-        box = self.box_head(cls_feat)  # [B,4,28,28]
+        box = self.box_head(box_feat)  # [B,4,28,28]
         box = box.flatten(2)           # [B,4,784]
 
         cls_map = self.cls_head(cls_feat)
         cls_map = cls_map.flatten(2)
 
         return box, cls_map, cls_btm
+
+
 
 # ===================== 全局配置(多尺度修改) =====================
 device = "cuda" if torch.cuda.is_available() else "cpu"
